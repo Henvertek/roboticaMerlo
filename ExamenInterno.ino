f6435e00e06813b43d3294f39b2b56ca7a855a28
Henvertek <juanwfer@gmail.com>
2017-02-06 00:16:34 -0300

    Update ExamenInterno
    Una versión que requiere menos reemplazos si se quieren cambiar los números. Y una prueba para ver cómo se boludea en GitHub...

--- ExamenInterno.ino
+++ ExamenInterno.ino
@@ -4,10 +4,12 @@ Serial.begin(9600);
 
 void loop(){
 int resultado = 0;
-int numero = 256;
-while(numero > 100){
+const int inicio = 256;
+const int maximo = 100;
+int numero = inicio;
+while(numero > maximo){
 	numero = numero / 2;
 	resultado = resultado + 1;
 }
-Serial.println("Efectuamos "+resultado+ " divisiones para que el numero 256 fuera menor que 100");
+Serial.println("Efectuamos "+resultado+ " divisiones para que el numero " +inicio+ " fuera menor que " +maximo);
 }
